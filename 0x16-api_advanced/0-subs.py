18b76a73a4148d8457f376667761da1447c8efdc
Imekaferdy <Imekaferdy20@gmail.com>
2022-04-28 00:23:52 -0700

    0x16-api_advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+contains the number of subscriber function
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+  #returns the number of subscribers for a given subreddit
+  if subreddit is None or type (subreddit) is not str:
+    return 0
+  else:
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit), headers={'User-agent': 'Python/requests:APIproject:\v1.0.0 (by /u/aaorrico23)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
+
