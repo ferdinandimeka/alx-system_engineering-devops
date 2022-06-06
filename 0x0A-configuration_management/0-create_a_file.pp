1c6ae5c1fe902380e7a59dfe6ef81e99b78da0e6
imekaferdy <imekaferdy20@gmail.com>
2022-06-06 14:33:34 -0700

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# Creates a file in /tmp
-
+# Create a file
 file { '/tmp/school':
-  ensure => file,
-  content => 'I love puppet',
-  group => 'www-data',
-  mode => '0744',
-  owner => 'www-data',
-  path => '/tmp/school',
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
