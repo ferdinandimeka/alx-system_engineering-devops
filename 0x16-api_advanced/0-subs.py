8fe7611ca0e34a4d7955f3ab415ab56ffe5a85de
imekaferdy <imekaferdy20@gmail.com>
2022-06-26 22:06:11 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""
-contains the number of subscriber function
-"""
+"""module which contains a function to bring information of Reddit API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-
-    # returns the number of subscribers for a given subreddit
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    else:
-        r = requests.get(
-            "http://www.reddit.com/r/{}/about.json".format(subreddit, end=""),
-            headers={
-                "User-agent": "Python/requests:APIproject:\v1.0.0 (by /u/aaor\
-rico23)"
-            },
-        ).json()
-        subs = r.get("data", {}).get("subscribers", 0)
-        return subs
+    """
+    function that queries the Reddit API and
+    returns the number of subscribers
+    """
+    response = requests.get(
+        "https://reddit.com/r/{}/about.json\
+".format(
+            subreddit
+        ),
+        headers={"User-agent": "botardo"},
+    )
+    return (
+        response.json()
+        .get("data")
+        .get(
+            "subscribers\
+"
+        )
+        if response.status_code != 404
+        else 0
+    )
