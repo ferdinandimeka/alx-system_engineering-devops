b1c673d0942955b35e6c35bfdc95d8ec9f303a21
imekaferdy <imekaferdy20@gmail.com>
2022-06-26 22:01:29 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,19 @@ contains the number of subscriber function
 """
 import requests
 
-def number_of_subscribers(subreddit):
-  #returns the number of subscribers for a given subreddit
-  if subreddit is None or type (subreddit) is not str:
-    return 0
-  else:
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit,\
-end=""), headers={'User-agent': 'Python/requests:APIproject:\v1.0.0 (by /u/aaor\
-rico23)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
 
+def number_of_subscribers(subreddit):
+
+    # returns the number of subscribers for a given subreddit
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    else:
+        r = requests.get(
+            "http://www.reddit.com/r/{}/about.json".format(subreddit, end=""),
+            headers={
+                "User-agent": "Python/requests:APIproject:\v1.0.0 (by /u/aaor\
+rico23)"
+            },
+        ).json()
+        subs = r.get("data", {}).get("subscribers", 0)
+        return subs
