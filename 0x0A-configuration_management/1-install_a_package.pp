4e2668fcff2d52d22750e8f34fae4136d7906eec
imekaferdy <imekaferdy20@gmail.com>
2022-06-26 23:18:11 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Installs puppet-lint, version 2.1.1
 
 package { 'puppet-lint':
-  ensure=> '2.1.0',
-  name=> 'puppet-lint',
-  provider=> 'gem',
+  ensure   => '2.1.0',
+  name     => 'puppet-lint',
+  provider => 'gem',
 }
