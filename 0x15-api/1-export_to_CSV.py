b53e9cf131aab0eb059730759c0a3e4675c809ef
imekaferdy <imekaferdy20@gmail.com>
2022-07-15 02:33:22 +0000

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,21 +2,25 @@
 """
 extend your Python script to export data in the CSV format
 """
-
 import csv
-import requests
+from requests import get
 from sys import argv
 
-if __name__ == '__main__':
-    endpoint = "https://jsonplaceholder.typicode.com/"
-    userId = argv[1]
-    user = requests.get(endpoint + "users/{}".
-                        format(userId), verify=False).json()
-    todo = requests.get(endpoint + "todos?userId={}".
-                        format(userId), verify=False).json()
-    with open("{}.csv".format(userId), 'w', newline='') as csvfile:
-        my_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todo:
-            my_writer.writerow([int(userId), user.get('username'),
-                                task.get('completed'),
-                                task.get('title')])
+
+def cvsWrite(user):
+    """writes to csv"""
+    data = get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        user)).json()
+    name = get('https://jsonplaceholder.typicode.com/users/{}'.format(
+        user)).json().get('username')
+    employ_data = open('{}.csv'.format(user), 'w')
+    cwrite = csv.writer(employ_data, quoting=csv.QUOTE_ALL)
+    for line in data:
+        lined = [line.get('userId'), name,
+                 line.get('completed'), line.get('title')]
+        cwrite.writerow(lined)
+    employ_data.close()
+
+
+if __name__ == "__main__":
+    cvsWrite(argv[1])
