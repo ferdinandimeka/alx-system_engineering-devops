aeb0ea4d8941debf9fd3e777af96580637617ab3
imekaferdy <imekaferdy20@gmail.com>
2022-07-15 18:45:07 +0000

    update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,26 +1,42 @@
 #!/usr/bin/python3
-"""
-extend your Python script to export data in the CSV format
-"""
-import csv
+"""Gather data from API"""
 from requests import get
-from sys import argv
-
-
-def cvsWrite(user):
-    """writes to csv"""
-    data = get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-        user)).json()
-    name = get('https://jsonplaceholder.typicode.com/users/{}'.format(
-        user)).json().get('username')
-    employ_data = open('{}.csv'.format(user), 'w')
-    cwrite = csv.writer(employ_data, quoting=csv.QUOTE_ALL)
-    for line in data:
-        lined = [line.get('userId'), name,
-                 line.get('completed'), line.get('title')]
-        cwrite.writerow(lined)
-    employ_data.close()
-
+from sys import argv, exit
+import csv
 
 if __name__ == "__main__":
-    cvsWrite(argv[1])
+    try:
+        id = argv[1]
+        is_integer = int(id)
+    except Exception:
+        exit()
+
+    url = "https://jsonplaceholder.typicode.com/"
+    url_user = url + "users?id=" + id
+    url_todos = url + "todos?userId=" + id
+
+    request_user = get(url_user)
+    request_todos = get(url_todos)
+    # Connection and have an access to the json
+    try:
+        jsuser = request_user.json()
+        jstodos = request_todos.json()
+    except ValueError:
+        print("No Json")
+
+    # Assing values
+    if jsuser and jstodos:
+        USER_ID = id
+        USERNAME = jsuser[0].get("username")
+
+        # export data in the csv file
+        with open(id + ".csv", "w", newline="") as csvfile:
+            csv_writer = csv.writer(
+                csvfile, delimiter=",", quotechar='"',
+                quoting=csv.QUOTE_ALL)
+            for task in jstodos:
+                TASK_COMPLETED_STATUS = task.get("completed")
+                TASK_TITLE = task.get("title")
+                csv_writer.writerow(
+                    [USER_ID, USERNAME,
+                     TASK_COMPLETED_STATUS, TASK_TITLE])
